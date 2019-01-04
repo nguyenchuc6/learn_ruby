7d1bb79fda8ca1790e7f5feb7b38769b486f07a7
Nguyen Chuc <nguyenminhchucit@gmail.com>
2019-01-04 09:10:35 +0700

    update file

--- basic/data_types.rb
+++ basic/data_types.rb
@@ -1,4 +1,19 @@
 class Data_Types
 
+  # number
+  count_student = 10;
+  puts count_student
+  # boolen
+  if nil
+    puts "true"
+  else
+    puts "flase"
+
+  end
+
+  # string
+  name = "nguyen minh chuc"
+  #
+  #   hash
   
 end
